89e3a8bd33a86b1d685a34a2f44657d4b858da58
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-08-20 20:50:50 +0200

    pokusaji

--- Web project/src/services/UserService.java
+++ Web project/src/services/UserService.java
@@ -1,4 +1,4 @@
-package servers;
+package services;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -18,35 +19,47 @@ import enums.Role;
 @Path("/user")
 public class UserService {
 	
+	@Context
 	ServletContext context;
+	@Context
 	HttpServletRequest request;
 	
+	public UserService() {
+		
+	}
+	
 	@GET
+	@Path("/bozepomozi")
+	public void hej() {
+		System.out.println("HEEJ USPELA SI");
+	}
+	
+	@POST
 	@Path("/login")
 	@Produces(MediaType.TEXT_HTML)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response login() {
+	public Response login(UserLoginDTO user) {
 		System.out.println("TU SAAAAAAAAAAAAAM");
 		UsersDAO users = getUsers();
 		
-		//User userForLogin = users.getUserByUsername(user.username);
+		User userForLogin = users.getUserByUsername(user.username);
 		
-		/*if(userForLogin == null) {
+		if(userForLogin == null) {
 			System.out.println("Nema usera");
 			return Response.status(Response.Status.BAD_REQUEST).entity("Korisnicko ime je pogresno!Probajte ponovo!!").build();
-		}*/
+		}
 		
-		/*if(!userForLogin.getPassword().equals(user.password)) {
+		if(!userForLogin.getPassword().equals(user.password)) {
 			System.out.println("losa sifra");
 			return Response.status(Response.Status.BAD_REQUEST).entity("Lozinka koju ste uneli je pogresna!Probajte ponovo!!").build();
-		}*/
+		}
 		
-		/*request.getSession().setAttribute("loginUser", userForLogin); //kacimo sesiju za korisnika
+		request.getSession().setAttribute("loginUser", userForLogin); //kacimo sesiju za korisnika
 		
 		if(userForLogin.getRole().equals(Role.ADMINISTRATOR)) {
 			System.out.println("admin sam");
 			return Response.status(Response.Status.ACCEPTED).entity("/WebShopREST/html/administrator_profil.html").build();
-		}*/
+		}
 		
 		System.out.println("idem na index.html");
 		return Response.status(Response.Status.ACCEPTED).entity("/WebShopREST/html/administrator_profil.html").build();
