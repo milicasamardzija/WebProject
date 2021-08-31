e74c811cbe5eb5a56e1ebbc3332c66ee701f7480
nevenaatic <atic.ra67.2018@uns.ac.rs>
2021-08-31 14:01:53 +0200

    kombinovana pretraga kod restorana u okviru profila

--- Web project/WebContent/app/kupacPrikazRestorana.js
+++ Web project/WebContent/app/kupacPrikazRestorana.js
@@ -7,8 +7,49 @@ Vue.component("restorani-kupac", {
 template: `
     <section>
         <div class="containerInfo">
+       
             <form action="#" >
             
+            <div class="row" style="margin-left: 50px;">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-12 p-0 search">
+                        <input type="text" class="form-control search-slt" placeholder="Naziv restorana">
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-12 p-0 search" >
+                        <input type="text" class="form-control search-slt" placeholder="Lokacija restorana">
+                    </div>
+                    <div class="dropdown col-lg-2 col-md-3 col-sm-12 p-0 filt">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" >
+                        Tip restorana
+                        </button>
+                        <span class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                        <button class="dropdown-item" type="button">Action</button>
+                        <button class="dropdown-item" type="button">Another action</button>
+                        <button class="dropdown-item" type="button">Something else here</button>
+                        </span>
+                    </div>
+                    <div class="dropdown col-lg-2 col-md-3 col-sm-12 p-0 filt">
+                        <button class="btn btn-secondary dropdown-toggle filters" type="button" data-toggle="dropdown">
+                        Ocena restorana
+                        </button>
+                        <div class="dropdown-menu">
+                        <button class="dropdown-item" type="button">Action</button>
+                        <button class="dropdown-item" type="button">Another action</button>
+                        <button class="dropdown-item" type="button">Something else here</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-12 btn-search">
+                        <button type="button" class="btn btn-danger wrn-btn">Pretrazi kombinovano</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+               
+
+
+
+
                 <div class="row-restaurants">
                     <div class = "col-with-picture">
                         <div class="col-picture">
