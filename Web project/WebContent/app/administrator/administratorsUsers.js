04739a50a2babd3b7868d9464a0997607d463ad4
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-09-03 12:20:20 +0200

    #izmene

--- Web project/WebContent/app/administrator/administratorsUsers.js
+++ Web project/WebContent/app/administrator/administratorsUsers.js
@@ -6,53 +6,53 @@ Vue.component("administrator-users", {
     },    
 template: `
 <div class="containerInfo">
-<div class="search-bar">
-    <div class="container">
-        <form action="#" >
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="row">
-                        <div class="col-lg-2 col-md-3 col-sm-12 p-0 search">
-                            <input type="text" class="form-control search-slt" placeholder="Ime">
-                        </div>
-                        <div class="col-lg-2 col-md-3 col-sm-12 p-0 search" >
-                            <input type="text" class="form-control search-slt" placeholder="Prezime">
-                        </div>
-                        <div class="col-lg-2 col-md-3 col-sm-12 p-0 search" >
-                          <input type="text" class="form-control search-slt" placeholder="Korisnicko ime">
-                        </div>
-                        <div class="dropdown col-lg-2 col-md-3 col-sm-12 p-0 filt">
-                            <button class="btn btn-secondary dropdown-toggle filters" type="button" data-toggle="dropdown">
-                              Uloga
-                            </button>
-                            <div class="dropdown-menu">
-                              <button class="dropdown-item" type="button">Action</button>
-                              <button class="dropdown-item" type="button">Another action</button>
-                              <button class="dropdown-item" type="button">Something else here</button>
+    <div class="search-bar">
+      <div class="container">
+          <form action="#" >
+              <div class="row">
+                  <div class="col-lg-12">
+                      <div class="row">
+                          <div class="col-lg-2 col-md-3 col-sm-12 p-0 search">
+                              <input type="text" class="form-control search-slt" placeholder="Ime">
+                          </div>
+                          <div class="col-lg-2 col-md-3 col-sm-12 p-0 search" >
+                              <input type="text" class="form-control search-slt" placeholder="Prezime">
+                          </div>
+                          <div class="col-lg-2 col-md-3 col-sm-12 p-0 search" >
+                            <input type="text" class="form-control search-slt" placeholder="Korisnicko ime">
+                          </div>
+                          <div class="dropdown col-lg-2 col-md-3 col-sm-12 p-0 filt">
+                              <button class="btn btn-secondary dropdown-toggle filters" type="button" data-toggle="dropdown">
+                                Uloga
+                              </button>
+                              <div class="dropdown-menu">
+                                <button class="dropdown-item" type="button">Action</button>
+                                <button class="dropdown-item" type="button">Another action</button>
+                                <button class="dropdown-item" type="button">Something else here</button>
+                              </div>
                             </div>
-                          </div>
-                          <div class="dropdown col-lg-1 col-md-3 col-sm-12 p-0 filt">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" >
-                              Tip korisnika
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                              <button class="dropdown-item" type="button">Action</button>
-                              <button class="dropdown-item" type="button">Another action</button>
-                              <button class="dropdown-item" type="button">Something else here</button>
+                            <div class="dropdown col-lg-1 col-md-3 col-sm-12 p-0 filt">
+                              <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" >
+                                Tip korisnika
+                              </button>
+                              <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <button class="dropdown-item" type="button">Action</button>
+                                <button class="dropdown-item" type="button">Another action</button>
+                                <button class="dropdown-item" type="button">Something else here</button>
+                              </div>
                             </div>
-                          </div>
-                          <div class="col-lg-2 col-md-3 col-sm-12 btn-search">
-                              <button type="button" class="btn btn-danger wrn-btn">Search</button>
-                          </div>
-                          <button type="button" class="btn btn-danger wrn-btn plusButton col-lg-1 col-md-3 col-sm-12"><span class="glyphicon glyphicon-plus"></span></button>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
-<!--tabela-->
-<div id="users">
+                            <div class="col-lg-2 col-md-3 col-sm-12 btn-search">
+                                <button type="button" class="btn btn-danger wrn-btn">Search</button>
+                            </div>
+                            <button type="button" class="btn btn-danger wrn-btn plusButton col-lg-1 col-md-3 col-sm-12"><span class="glyphicon glyphicon-plus"></span></button>
+                      </div>
+                  </div>
+              </div>
+          </form>
+      </div>
+
+  <!--tabela-->
+  <div id="users">
     <table class="table table-hover">
         <thead>
           <tr>
@@ -72,14 +72,15 @@ template: `
             <td>{{user.username}}</td>
             <td>{{user.points}}</td>
             <td>{{user.role}}</td>
-            <td>{{user.typeCustomer.type}}</td>
+            <td>{{user.role}}</td>
             <div>
               <td><button type="button" class="btn btn-secondary">Izmeni</button></td>
               <td><button type="button" class="btn btn-secondary">Izbrisi</button></td>
             </div>
           </tr>
         </tbody>
-      </table>
+    </table>
+  </div>
 </div>
 `,
     mounted() {
