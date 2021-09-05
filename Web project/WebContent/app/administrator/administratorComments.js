513f1544c7b20b9054a6f66d87cb9253b749ac5d
Milica <sammilica99@gmail.com>
2021-09-05 17:26:40 +0200

    #komentari

--- Web project/WebContent/app/administrator/administratorComments.js
+++ Web project/WebContent/app/administrator/administratorComments.js
@@ -6,14 +6,14 @@ Vue.component("administrator-comments", {
     },    
 template: `
     <div class="containerInfo">
-        <div class="media">
+        <div class="media" v-for="comment in allComments">
             <div class="media-left media-top">
             <img src="../pictures/korisnik.png" class="media-object" style="width:60px">
             </div>
             <div class="media-body">
-            <h4 class="media-heading">Ciao</h4>
-            <p style="font-weight: 300; font-style: italic;">pera123</p>
-            <p>Sve je bilo super</p>
+            <h4 class="media-heading">{{comment.restaurantName}}</h4>
+            <p style="font-weight: 300; font-style: italic;">{{comment.username}}</p>
+            <p>{{comment.text}}</p>
             <p>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
