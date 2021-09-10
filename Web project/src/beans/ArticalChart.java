fc3eb480c95d6ea8c312a5606628046742047bb3
nevenaatic <atic.ra67.2018@uns.ac.rs>
2021-09-11 00:48:01 +0200

    korpa start

--- Web project/src/beans/ArticalChart.java
+++ Web project/src/beans/ArticalChart.java
@@ -3,4 +3,37 @@ package beans;
 public class ArticalChart {
 	private int idArtical;
 	private int quantity;
+	private int idCustomer;
+	
+	
+	public int getIdArtical() {
+		return idArtical;
+	}
+	public void setIdArtical(int idArtical) {
+		this.idArtical = idArtical;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getIdCustomer() {
+		return idCustomer;
+	}
+	public void setIdCustomer(int idCustomer) {
+		this.idCustomer = idCustomer;
+	}
+	public ArticalChart(int idArtical, int quantity, int idCustomer) {
+		super();
+		this.idArtical = idArtical;
+		this.quantity = quantity;
+		this.idCustomer = idCustomer;
+	}
+	public ArticalChart() {
+		super();
+	}
+	
+	
+	
 }
--- Web project/src/beans/Chart.java
+++ Web project/src/beans/Chart.java
@@ -6,5 +6,40 @@ public class Chart {
 	private int id;
 	private ArrayList<Integer> articalsIds;
 	private String idCustomer;
-	private double price;
+		private double price;
+		
+	public Chart() {}	
+	
+	public Chart(int id, ArrayList<Integer> articalsIds, String idCustomer, double price) {
+		super();
+		this.id = id;
+		this.articalsIds = articalsIds;
+		this.idCustomer = idCustomer;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArrayList<Integer> getArticalsIds() {
+		return articalsIds;
+	}
+	public void setArticalsIds(ArrayList<Integer> articalsIds) {
+		this.articalsIds = articalsIds;
+	}
+	public String getIdCustomer() {
+		return idCustomer;
+	}
+	public void setIdCustomer(String idCustomer) {
+		this.idCustomer = idCustomer;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
